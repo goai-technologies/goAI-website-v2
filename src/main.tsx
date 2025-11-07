12c661776b1cfa44f9ac7d0c0e44f2f2f4c3480c
Deepak Belavadi <db@goai.software>
2025-11-07 12:42:55 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
+//to force commit for deployment
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
